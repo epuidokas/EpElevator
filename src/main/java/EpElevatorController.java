05f93655f6d60a4fb5b606bcce69c39e370b87c2
Eric Puidokas <ep@yahoo-inc.com>
2012-05-01 09:06:21 -0700

    Simple logger and added register to tests

--- src/main/java/EpElevatorController.java
+++ src/main/java/EpElevatorController.java
@@ -1,17 +1,25 @@
 public class EpElevatorController implements ElevatorController {
 
+    Logger logger = new Logger();
+
     public EpElevatorController()
     {
-
+        logger.log("EpElevatorController started.");
     }
 
     public void registerElevator(Elevator elevator)
     {
-
+        logger.log("New elevator registered.");
     }
 
     public void registerButton(Button button)
     {
+        logger.log("New button registered.");
+    }
 
+    public class Logger {
+        public void log(String msg) {
+            System.out.println(msg);
+        }
     }
 }
--- src/test/java/EpElevatorControllerTest.java
+++ src/test/java/EpElevatorControllerTest.java
@@ -12,6 +12,17 @@ public class EpElevatorControllerTest extends junit.framework.TestCase {
 
     public void testElevatorManager() throws Exception {
         EpElevatorController epElevatorController = new EpElevatorController();
+
+        // Register 2 elevators
+        for (Integer i = 0; i < 2; i++) {
+            epElevatorController.registerElevator(new PhysicalElevator());
+        }
+
+        // Register 20 buttons (10 floors)
+        for (Integer i = 0; i < 20; i++) {
+            epElevatorController.registerButton(new PhysicalButton());
+        }
+
     }
 
     class PhysicalElevator implements Elevator {
